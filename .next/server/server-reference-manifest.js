cd44821d42af2942b6cda87a800b3432b531a574
Frank-Luo69 <2595331257@qq.com>
2025-08-20 10:39:58 +0000

    ci: add GitHub Actions (build + unit tests) and optional e2e job

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"PKSRu2aHZony1EOIWQSQ35nNJ6g64e7wY1xyx51Bs+M=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e36YJ+snC9pONGCXBaA1kbwRtqcyKUdRmpsaJ/tLr60=\"\n}"
